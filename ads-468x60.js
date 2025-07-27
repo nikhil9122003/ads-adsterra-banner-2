73d301b3d925f0d0e321e94a91a33ef2559f73c8
nikhil9122003 <166285467+nikhil9122003@users.noreply.github.com>
2025-07-27 11:03:12 +0530

    Create ads-468x60.js

---
+++ ads-468x60.js
@@ -0,0 +1,14 @@
+(function() {
+  var atOptions = {
+    'key' : '39eb290b2635f443c9d7600773933688',
+    'format' : 'iframe',
+    'height' : 60,
+    'width' : 468,
+    'params' : {}
+  };
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.src = '//www.highperformanceformat.com/39eb290b2635f443c9d7600773933688/invoke.js';
+  s.async = true;
+  document.currentScript.parentNode.insertBefore(s, document.currentScript);
+})();
