e234702cd19a107a0b30cc22deabbacc446250d3
nikhil9122003 <166285467+nikhil9122003@users.noreply.github.com>
2025-07-27 11:45:54 +0530

    Update ads-468x60.js

--- ads-468x60.js
+++ ads-468x60.js
@@ -1,14 +1,28 @@
 (function() {
-  var atOptions = {
-    'key' : '39eb290b2635f443c9d7600773933688',
-    'format' : 'iframe',
-    'height' : 60,
-    'width' : 468,
-    'params' : {}
-  };
-  var s = document.createElement('script');
-  s.type = 'text/javascript';
-  s.src = '//www.highperformanceformat.com/39eb290b2635f443c9d7600773933688/invoke.js';
-  s.async = true;
-  document.currentScript.parentNode.insertBefore(s, document.currentScript);
+  var container = document.getElementById("ad-468x60");
+
+  if (container) {
+    // Set Adsterra configuration
+    var configScript = document.createElement('script');
+    configScript.type = 'text/javascript';
+    configScript.innerHTML = `
+      atOptions = {
+        'key' : '39eb290b2635f443c9d7600773933688',
+        'format' : 'iframe',
+        'height' : 60,
+        'width' : 468,
+        'params' : {}
+      };
+    `;
+    container.appendChild(configScript);
+
+    // Load the invoke.js script
+    var adScript = document.createElement('script');
+    adScript.type = 'text/javascript';
+    adScript.src = 'https://www.highperformanceformat.com/39eb290b2635f443c9d7600773933688/invoke.js';
+    adScript.async = true;
+    container.appendChild(adScript);
+  } else {
+    console.warn('Ad container #ad-468x60 not found.');
+  }
 })();
